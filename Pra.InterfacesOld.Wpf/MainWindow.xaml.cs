717949815e1e92f2ac242161b896d096e0dc848b
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2021-11-29 20:20:09 +0100

    Add project files.

---
+++ Pra.InterfacesOld.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pra.InterfacesOld.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddAppliance_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LstAppliances_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnApplianceOn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnApplianceOff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAllOn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAllOff_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAllVolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAllVolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
